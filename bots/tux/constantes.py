2a9483fae59d586bd62228ea1f4626c2dc62855f
ribt <ribt115@gmail.com>
2018-08-12 21:19:31 +0200

    Tux v2.0 !!!!!!!!!!!!!

---
+++ bots/tux/constantes.py
@@ -0,0 +1,141 @@
+fast = """```  ______        _   
+ |  ____|      | |  
+ | |__ __ _ ___| |_ 
+ |  __/ _` / __| __|
+ | | | (_| \__ \ |_ 
+ |_|  \__,_|___/\__|```
+ """
+
+aide_fast = fast +  u"""**Fast** est un jeu où vous devez retaper la chaîne de caractères choisie par le bot le plus rapidement possible.
+__Niveau **1**__ : 10 à 20 lettres minuscules
+__Niveau **2**__ : 10 à 20 lettres minuscules ou majuscules
+__Niveau **3**__ : 10 à 20 lettres minuscules (avec ou sans accent), majuscules ou caractères numériques
+__Niveau **4**__ : 10 à 20 lettres minuscules (avec ou sans accent), majuscules, caractères numériques ou spéciaux
+__Niveau **5**__ : 20 à 30 lettres minuscules (avec ou sans accent), majuscules, caractères numériques ou spéciaux
+Bon courage \N{SMILING FACE WITH HORNS}"""
+
+caracteres = ['abcdefghijklmnopqrstuvwxyz',
+              'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+              'ABCDEFGHIJKLMNOPQRSTUVWXYZaàbcçdeêëéèfghijklmnopqrstuùvwxyz0123456789',
+              'ABCDEFGHIJKLMNOPQRSTUVWXYZaàbcçdeêëéèfghijklmnopqrstuùvwxyz0123456789&"#\'{([-|_\\)]°+=}$*?,.;/:!']
+
+feeds = ["https://korben.info/feed",
+         "https://www.begeek.fr/feed",
+         "http://blogmotion.fr/feed",
+         "http://www.framboise314.fr/feed/", "http://www.journaldugeek.com/feed/",
+         "https://thehackernews.com/feeds/posts/default",
+         "https://usbeketrica.com/rss",
+         "https://www.lemondeinformatique.fr/flux-rss/",
+         "http://www.01net.com/rss/actualites/"]
+
+pendu = ["""```
+                       
+                        
+                        
+                         
+                          
+                          
+                         
+                        
+         
+        ============```""", """```
+            
+           ||          
+           ||          
+           ||          
+           ||         
+           ||         
+           ||
+           ||
+           ||
+        ============```""",
+          """```
+            ============
+           ||        
+           ||        
+           ||       
+           ||       
+           ||        
+           ||
+           ||
+           ||
+        ============```""",
+          """```
+            ============
+           ||  /      
+           || /       
+           ||/       
+           ||       
+           ||       
+           ||
+           ||
+           ||
+        ============```""",
+          """```
+            ============
+           ||  /      |
+           || /       |
+           ||/        
+           ||        
+           ||        
+           ||
+           ||
+           ||
+        ============```""",
+          """```
+            ============
+           ||  /      |
+           || /       |
+           ||/        O
+           ||         |
+           ||        
+           ||
+           ||
+           ||
+        ============```""",
+          """```
+            ============
+           ||  /      |
+           || /       |
+           ||/        O/
+           ||         |
+           ||        
+           ||
+           ||
+           ||
+        ============```""",
+          r"""```
+            ============
+           ||  /      |
+           || /       |
+           ||/       \O/
+           ||         |
+           ||        
+           ||
+           ||
+           ||
+        ============```""",
+          r"""```
+            ============
+           ||  /      |
+           || /       |
+           ||/       \O/
+           ||         |
+           ||          \
+           ||
+           ||
+           ||
+        ============```""",
+          r"""```
+            ============
+           ||  /      |
+           || /       |
+           ||/       \O/
+           ||         |
+           ||        / \
+           ||
+           ||
+           ||
+        ============```"""]
+
+ytCategories = {'1': 'Film & Animation', '2': 'Autos & Vehicles', '10': 'Music', '15': 'Pets & Animals', '17': 'Sports', '18': 'Short Movies', '19': 'Travel & Events', '20': 'Gaming', '21': 'Videoblogging', '22': 'People & Blogs', '23': 'Comedy', '24': 'Entertainment', '25': 'News & Politics', '26': 'Howto & Style', '27': 'Education', '28': 'Science & Technology', '30': 'Movies', '31': 'Anime/Animation', '32': 'Action/Adventure', '33': 'Classics', '34': 'Comedy', '35': 'Documentary', '36': 'Drama', '37': 'Family', '38': 'Foreign', '39': 'Horror', '40': 'Sci-Fi/Fantasy', '41': 'Thriller', '42': 'Shorts', '43': 'Shows', '44': 'Trailers'}
--- bots/tux/fonctions.py
+++ bots/tux/fonctions.py
@@ -6,55 +6,55 @@ import json
 from calendar import timegm
 
 def joliStr(n):
-  if isinstance(n, float) :
-    ent, dec = str(n).split(".")
-    ent = int(ent)
-  elif isinstance(n, str) :
-    if "." in n :
-      ent, dec = n.split(".")
-      ent = int(ent)
-    else : ent, dec = int(n), ""
-  else : 
-    ent = n
-    dec = 0
+    if isinstance(n, float) :
+        ent, dec = str(n).split(".")
+        ent = int(ent)
+    elif isinstance(n, str) :
+        if "." in n :
+            ent, dec = n.split(".")
+            ent = int(ent)
+        else : ent, dec = int(n), ""
+    else : 
+        ent = n
+        dec = 0
 
-  result = ""
-  while ent >= 1000:
-    ent, r = divmod(ent, 1000)
-    if ent > 0 and r < 100 :
-      if r < 10 : result = " 00" + str(r) + result
-      else : result = " 0" + str(r) + result
-    else : result = " " + str(r) + result
+    result = ""
+    while ent >= 1000:
+        ent, r = divmod(ent, 1000)
+        if ent > 0 and r < 100 :
+            if r < 10 : result = " 00" + str(r) + result
+            else : result = " 0" + str(r) + result
+        else : result = " " + str(r) + result
 
-  result = str(ent) + result
+    result = str(ent) + result
 
-  if dec :
-    result += "," 
-    i = 0
-    while i < len(dec):
-      try :
-        result += dec[i]
-        result += dec[i+1]
-        result += dec[i+2]
-        result += " "
-      except IndexError : pass
-      i += 3
-  return result
+    if dec :
+        result += "," 
+        i = 0
+        while i < len(dec):
+            try :
+                result += dec[i]
+                result += dec[i+1]
+                result += dec[i+2]
+                result += " "
+            except IndexError : pass
+            i += 3
+    return result
 
 def getUrl(url) :
-  req = Request(url, headers={'User-Agent': "Je n'suis pas un robot (enfin si mais un gentil ^^) !"})
-  result = urlopen(req)
-  result = unescape(result.read().decode("utf-8"))
-  return json.loads(result)
+    req = Request(url, headers={'User-Agent': "Je n'suis pas un robot (enfin si mais un gentil ^^) !"})
+    result = urlopen(req)
+    result = unescape(result.read().decode("utf-8"))
+    return json.loads(result)
 
 async def actu():
-  await client.wait_until_ready()
-  channel = discord.Object(id='420635603592413185')
-  while not client.is_closed:
-      dernier = time.time()
-      await asyncio.sleep(10*60) # 10 minutes
-      for feed in feeds :
-          rss = feedparser.parse(feed)
-          for i in rss['entries'] :
-              if timegm(i['published_parsed']) > dernier :
-                  await client.send_message(channel, i['link'])
+    await client.wait_until_ready()
+    channel = discord.Object(id='420635603592413185')
+    while not client.is_closed:
+        dernier = time.time()
+        await asyncio.sleep(10*60) # 10 minutes
+        for feed in feeds :
+            rss = feedparser.parse(feed)
+            for i in rss['entries'] :
+                if timegm(i['published_parsed']) > dernier :
+                    await client.send_message(channel, i['link'])
